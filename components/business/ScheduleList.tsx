8c860493cbf0f7d64b086cc96347801efd4f0c24
Rsport <rsport19movetebien@gmail.com>
2026-01-28 16:43:21 -0300

    Add files via upload

--- components/business/ScheduleList.tsx
+++ components/business/ScheduleList.tsx
@@ -21,7 +21,7 @@ export const ScheduleList = ({ slots }: ScheduleListProps) => {
                             fill
                             style={{ objectFit: 'cover' }}
                         />
-                        <span className={styles.badge}>{slot.title === 'Funcional' ? 'Trabajos aeróbico y de fuerza' : 'Personalizado'}</span>
+                        <span className={styles.badge}>{slot.title === 'Funcional' ? 'Trabajos aeróbicos y de fuerza' : 'Personalizado'}</span>
                     </div>
                     <div className={styles.cardContent}>
                         <h3 className={styles.classTitle}>{slot.title}</h3>
